836b89b9dbedc88b1ed9478a30f6674325e5bcb3
Adrien Wehrle <adrien.wehrle@hotmail.fr>
2020-10-09 15:53:55 +0200

    initial commit

---
+++ arctic_oscillation_index.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Adrien Wehrlé, GEUS (Geological Survey of Denmark and Greenland)
+
+"""
+
+
+import numpy as np
+import pandas as pd
+
+https://www.ncdc.noaa.gov/teleconnections/ao/
