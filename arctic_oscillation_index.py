d45f36b2ed1a4f11a3a5e7b6990932d5d4f6050c
Adrien Wehrle <adrien.wehrle@hotmail.fr>
2020-10-09 18:14:11 +0200

    initial commit

--- arctic_oscillation_index.py
+++ arctic_oscillation_index.py
@@ -8,5 +8,13 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-https://www.ncdc.noaa.gov/teleconnections/ao/
+#https://www.ncdc.noaa.gov/teleconnections/ao/
+#https://cds.climate.copernicus.eu/
+#ERA5 monthly averaged data on single levels from 1979 to present
+
+ao_idx=pd.read_csv('C:/Users/Pascal/Desktop/UGAM2/CIA/climatic-modes-arctic/AO_index_monthly.txt')
+
+ao_idx['Time']=pd.to_datetime(ao_idx.Date,format='%Y%m')
+
