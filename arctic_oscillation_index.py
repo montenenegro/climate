df7388b25f3c516b75e26d4800b8b94afbf33067
Adrien Wehrle <adrien.wehrle@hotmail.fr>
2020-10-09 18:57:31 +0200

    era5 netcdf

--- arctic_oscillation_index.py
+++ arctic_oscillation_index.py
@@ -9,6 +9,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import xarray as xr
 
 #https://www.ncdc.noaa.gov/teleconnections/ao/
 #https://cds.climate.copernicus.eu/
@@ -18,3 +19,5 @@ ao_idx=pd.read_csv('C:/Users/Pascal/Desktop/UGAM2/CIA/climatic-modes-arctic/AO_i
 
 ao_idx['Time']=pd.to_datetime(ao_idx.Date,format='%Y%m')
 
+
+era = xr.open_dataset("C:/Users/Pascal/Desktop/UGAM2/CIA/adaptor.mars.internal-1602255451.139694-24165-26-eecb89cc-17e1-4466-b8a2-11d905ef570a.nc")
